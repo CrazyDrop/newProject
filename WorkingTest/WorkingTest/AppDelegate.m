4d6b355f32f451e26de641cb4c6bca287800ef68
Apple <apple@AppledeMacBook-Pro.local>
2014-08-20 09:42:38 +0800

    commit  test

--- WorkingTest/WorkingTest/AppDelegate.m
+++ WorkingTest/WorkingTest/AppDelegate.m
@@ -12,9 +12,13 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
+//  test commit
+    
+    
     self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
     // Override point for customization after application launch.
     self.window.backgroundColor = [UIColor whiteColor];
+    self.window.backgroundColor = [UIColor redColor];
     [self.window makeKeyAndVisible];
     return YES;
 }
