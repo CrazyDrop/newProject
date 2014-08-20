c8b77d5bba608c029894bb0488e89e967edc6117
Apple <apple@AppledeMacBook-Pro.local>
2014-08-20 09:33:19 +0800

    新增工程文件

---
+++ WorkingTest/WorkingTest/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  WorkingTest
+//
+//  Created by Apple on 14-8-20.
+//  Copyright (c) 2014年 zhangchaoqun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ WorkingTest/WorkingTestTests/WorkingTestTests.m
@@ -0,0 +1,34 @@
+//
+//  WorkingTestTests.m
+//  WorkingTestTests
+//
+//  Created by Apple on 14-8-20.
+//  Copyright (c) 2014年 zhangchaoqun. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface WorkingTestTests : XCTestCase
+
+@end
+
+@implementation WorkingTestTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
